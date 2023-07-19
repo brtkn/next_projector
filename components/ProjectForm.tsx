693321eb14562d8263727368466ae8be7404cdd2
brtkn <haha>
2023-07-19 23:45:29 +0300

    .

--- components/ProjectForm.tsx
+++ components/ProjectForm.tsx
@@ -1,14 +1,15 @@
 "use client";
 
-import { FormState, ProjectInterface, SessionInterface } from "@/common.types";
 import Image from "next/image";
-import { ChangeEvent, useState } from "react";
-import FormField from "./FormField";
-import { categoryFilters } from "@/constants";
-import CustomMenu from "./CustomMenu";
-import Button from "./Button";
-import { createNewProject, fetchToken, updateProject } from "@/lib/actions";
+import React, { ChangeEvent, FormEvent, useState } from "react";
 import { useRouter } from "next/navigation";
+
+import FormField from "./FormField";
+import Button from "./Button";
+import CustomMenu from "./CustomMenu";
+import { categoryFilters } from "@/constant";
+import { updateProject, createNewProject, fetchToken } from "@/lib/actions";
+import { FormState, ProjectInterface, SessionInterface } from "@/common.types";
 
 type Props = {
   type: string;
@@ -19,10 +20,48 @@ type Props = {
 const ProjectForm = ({ type, session, project }: Props) => {
   const router = useRouter();
 
-  const handleFormSubmit = async (e: React.FormEvent) => {
+  const [submitting, setSubmitting] = useState<boolean>(false);
+  const [form, setForm] = useState<FormState>({
+    title: project?.title || "",
+    description: project?.description || "",
+    image: project?.image || "",
+    liveSiteUrl: project?.liveSiteUrl || "",
+    githubUrl: project?.githubUrl || "",
+    category: project?.category || "",
+  });
+
+  const handleStateChange = (fieldName: keyof FormState, value: string) => {
+    setForm((prevForm) => ({ ...prevForm, [fieldName]: value }));
+  };
+
+  const handleChangeImage = (e: ChangeEvent<HTMLInputElement>) => {
     e.preventDefault();
-    // Form submission logic here
-    setIsSubmitting(true);
+
+    const file = e.target.files?.[0];
+
+    if (!file) return;
+
+    if (!file.type.includes("image")) {
+      alert("Please upload an image!");
+
+      return;
+    }
+
+    const reader = new FileReader();
+
+    reader.readAsDataURL(file);
+
+    reader.onload = () => {
+      const result = reader.result as string;
+
+      handleStateChange("image", result);
+    };
+  };
+
+  const handleFormSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+
+    setSubmitting(true);
 
     const { token } = await fetchToken();
 
@@ -32,120 +71,95 @@ const ProjectForm = ({ type, session, project }: Props) => {
 
         router.push("/");
       }
+
       if (type === "edit") {
         await updateProject(form, project?.id as string, token);
+
         router.push("/");
       }
     } catch (error) {
-      console.log(error);
+      alert(
+        `Failed to ${
+          type === "create" ? "create" : "edit"
+        } a project. Try again!`
+      );
     } finally {
-      setIsSubmitting(false);
+      setSubmitting(false);
     }
   };
-
-  const handleChangeImage = (e: ChangeEvent<HTMLInputElement>) => {
-    e.preventDefault();
-
-    const file = e.target.files?.[0];
-    if (!file) return;
-
-    if (!file.type.includes("image")) {
-      return alert("Please upload an image file.");
-    }
-
-    const reader = new FileReader();
-    reader.readAsDataURL(file);
-
-    reader.onload = () => {
-      const result = reader.result as string;
-
-      //Update image with result.
-      handleStateChange("image", result);
-    };
-  };
-
-  const handleStateChange = (fieldName: string, value: string) => {
-    setForm((prevState) => ({ ...prevState, [fieldName]: value }));
-  };
-
-  const [isSubmitting, setIsSubmitting] = useState(false);
-
-  const [form, setForm] = useState({
-    title: project?.title || "",
-    description: project?.description || "",
-    image: project?.image || "",
-    liveSiteUrl: project?.liveSiteUrl || "",
-    githubUrl: project?.githubUrl || "",
-    category: project?.category || "",
-  });
 
   return (
     <form onSubmit={handleFormSubmit} className="flexStart form">
       <div className="flexStart form_image-container">
         <label htmlFor="poster" className="flexCenter form_image-label">
-          {!form.image && "Choose your poster for your project"}
+          {!form.image && "Choose a poster for your project"}
         </label>
         <input
-          type="file"
           id="image"
+          type="file"
           accept="image/*"
-          required={type === "create"}
+          required={type === "create" ? true : false}
           className="form_image-input"
-          onChange={handleChangeImage}
+          onChange={(e) => handleChangeImage(e)}
         />
         {form.image && (
           <Image
-            src={form.image}
+            src={form?.image}
             className="sm:p-10 object-contain z-20"
-            alt="Project Poster"
+            alt="image"
             fill
           />
         )}
       </div>
+
       <FormField
         title="Title"
         state={form.title}
-        placeholder="Projector"
+        placeholder="Flexibble"
         setState={(value) => handleStateChange("title", value)}
       />
+
       <FormField
         title="Description"
         state={form.description}
         placeholder="Showcase and discover remarkable developer projects."
+        isTextArea
         setState={(value) => handleStateChange("description", value)}
       />
+
       <FormField
         type="url"
-        title="Website Url"
+        title="Website URL"
         state={form.liveSiteUrl}
-        placeholder="https://google.com"
+        placeholder="https://jsmastery.pro"
         setState={(value) => handleStateChange("liveSiteUrl", value)}
       />
+
       <FormField
         type="url"
-        title="GitHub Url"
+        title="GitHub URL"
         state={form.githubUrl}
-        placeholder="https://github.com"
+        placeholder="https://github.com/adrianhajdin"
         setState={(value) => handleStateChange("githubUrl", value)}
       />
 
-      {/* Custom Input Category */}
       <CustomMenu
         title="Category"
         state={form.category}
         filters={categoryFilters}
         setState={(value) => handleStateChange("category", value)}
       />
+
       <div className="flexStart w-full">
         <Button
           title={
-            isSubmitting
+            submitting
               ? `${type === "create" ? "Creating" : "Editing"}`
               : `${type === "create" ? "Create" : "Edit"}`
           }
           type="submit"
-          leftIcon={isSubmitting ? "" : "/plus.svg"}
-          isSubmitting={isSubmitting}
+          leftIcon={submitting ? "" : "/plus.svg"}
+          submitting={submitting}
         />
       </div>
     </form>
